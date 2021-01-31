b04f162d3faa9fcaa1d3f972d10e5b9cc9614507
wellingtonx <wlss.lucas@gmail.com>
2021-01-31 13:02:51 -0300

    creates organizations resource with index and create actions

--- spec/requests/health_check_spec.rb
+++ spec/requests/health_check_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe "HealthChecks", type: :request do
   describe "GET /" do
     context "when client requests html" do
-      it "should return status 200" do
+      it "should return proper message" do
         get root_path
         expect(response.body).to eq("I am up and running!")
       end
     end
 
     context "when client requests json" do
-      it "should return status 200" do
+      it "should return proper message" do
         get root_path, headers: { "Accept": "application/json" }
         expect(response.body).to eq("I am up and running!")
       end
