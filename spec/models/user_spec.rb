d03887cb4577bfc4df1929ed00ecdf710ccd8c8f
wellingtonx <wlss.lucas@gmail.com>
2021-01-31 16:11:53 -0300

    creates organizations and users association

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { build(:user) }
+  let!(:user) { build(:user, :with_organization) }
 
   it "is not valid without an email" do
     user.email = nil
+
+    expect(user).to_not be_valid
+  end
+
+  it "is not valid without at least one organization" do
+    user.organizations = []
 
     expect(user).to_not be_valid
   end
