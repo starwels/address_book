34c5237ff4126a9018c84e4c531db31ebe4b287c
wellingtonx <wlss.lucas@gmail.com>
2021-01-31 13:49:32 -0300

    creates user model and registrations create action

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :organizations, only: [:index, :create]
+      resources :registrations, only: [:create]
     end
   end
 end
