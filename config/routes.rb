24132205aa979867aeb34a0b50d39c6073d74d2e
wellingtonx <wlss.lucas@gmail.com>
2021-01-31 22:28:22 -0300

    creates authentication and refactors tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :authentications, only: [:create]
       resources :organizations, only: [:index, :create]
       resources :registrations, only: [:create]
     end
--- spec/requests/api/v1/organizations_spec.rb
+++ spec/requests/api/v1/organizations_spec.rb
@@ -6,24 +6,29 @@ RSpec.describe "Api::V1::Organizations", type: :request do
 
   describe "GET /api/v1/organizations" do
     let!(:organizations) { create_list(:organization, 3) }
+    let(:user) { create(:user, organizations: organizations) }
+    let(:headers) { { 'Authorization' => token_generator(user) } }
 
     it "lists all organizations" do
-      get api_v1_organizations_path
+      get api_v1_organizations_path, headers: headers
       expect(json_body[:organizations].size).to eq(3)
     end
   end
 
   describe "POST /api/v1/organizations" do
+    let(:user) { create(:user, :with_organization) }
+    let(:headers) { { 'Authorization' => token_generator(user) } }
+
     context 'when params are valid' do
       let(:organization_params) { attributes_for(:organization) }
 
       it "returns status 201" do
-        post api_v1_organizations_path, params: { organization: organization_params }
+        post api_v1_organizations_path, headers: headers, params: { organization: organization_params }
         expect(response).to have_http_status(201)
       end
 
       it "returns the created organization" do
-        post api_v1_organizations_path, params: { organization: organization_params }
+        post api_v1_organizations_path, headers: headers, params: { organization: organization_params }
         expect(json_body[:organization][:name]).to eq(organization_params[:name])
       end
     end
@@ -32,12 +37,12 @@ RSpec.describe "Api::V1::Organizations", type: :request do
       let(:organization_params) { { name: nil } }
 
       it "returns status 422" do
-        post api_v1_organizations_path, params: { organization: organization_params }
+        post api_v1_organizations_path, headers: headers, params: { organization: organization_params }
         expect(response).to have_http_status(422)
       end
 
       it "returns errors" do
-        post api_v1_organizations_path, params: { organization: organization_params }
+        post api_v1_organizations_path, headers: headers, params: { organization: organization_params }
         expect(json_body).to include(:errors)
       end
     end
