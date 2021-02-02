8adced1c52f6d2d95aed9a127d95ea41a418d0bb
wellingtonx <wlss.lucas@gmail.com>
2021-02-02 16:26:30 -0300

    refactoring authenticable module

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -22,8 +22,4 @@ class Api::V1::OrganizationsController < ApplicationController
   def organization_params
     params.require(:organization).permit(:name)
   end
-
-  def authenticate_admin!
-    unauthorized_entity unless current_user.admin?
-  end
 end
