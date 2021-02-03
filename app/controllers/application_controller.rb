3bce4d4af22db0c7d5c694c8a4b17cf38e4cf485
wellingtonx <wlss.lucas@gmail.com>
2021-02-02 21:04:47 -0300

    bug fix of authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::API
   include Authenticable
 
   before_action :authenticate_user!
+
+  attr_reader :current_user
+
+  def current_user
+    @current_user ||= get_current_user
+  end
 end
