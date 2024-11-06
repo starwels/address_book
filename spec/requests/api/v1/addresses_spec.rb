9e4a97cf527089602fe7da26f212d87f19146287
Wellington Santos <wlss.lucas@gmail.com>
2024-11-06 02:49:50 -0300

    Add or update spec/requests/api/v1/addresses_spec.rb

---
+++ spec/requests/api/v1/addresses_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Addresses', type: :request do
+  describe 'POST /api/v1/addresses' do
+    let(:user) { create(:user) }
+    let(:valid_attributes) { { address: { street: '123 Main St', city: 'Anytown', state: 'CA', zip: '12345', country: 'USA', user_id: user.id } } }
+
+    context 'with valid parameters' do
+      it 'creates a new Address' do
+        expect {
+          post api_v1_addresses_path, params: valid_attributes
+        }.to change(Address, :count).by(1)
+
+        expect(response).to have_http_status(:created)
+        expect(json['address']).to include('street' => '123 Main St')
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not create a new Address' do
+        expect {
+          post api_v1_addresses_path, params: { address: { street: '' } }
+        }.not_to change(Address, :count)
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+end
