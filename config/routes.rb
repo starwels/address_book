c6a6773b436ae60657071cc0e5847c35f5f58b40
Wellington Santos <wlss.lucas@gmail.com>
2024-11-06 02:24:16 -0300

    Add or update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,3 +10,16 @@ Rails.application.routes.draw do
     end
   end
 end
+
+Rails.application.routes.draw do
+  root 'health_check#index'
+  namespace :api do
+    namespace :v1 do
+      resources :authentications, only: [:create]
+      resources :contacts, only: [:index, :create, :update, :destroy]
+      resources :organizations, only: [:index, :create]
+      resources :registrations, only: [:create]
+      resources :addresses, only: [:create]
+    end
+  end
+end
