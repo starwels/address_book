377dcb4ae1920666b2dd55e8911af4d69c4a7ec1
Wellington Santos <wlss.lucas@gmail.com>
2024-11-06 02:07:46 -0300

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  root 'health_check#index'
-
   namespace :api do
     namespace :v1 do
-      resources :authentications, only: [:create]
-      resources :contacts, only: [:index, :create, :update, :destroy]
-      resources :organizations, only: [:index, :create]
-      resources :registrations, only: [:create]
+      resources :addresses, only: [:create]
     end
   end
 end
